ce8ac2235c8ddff46c11af2a7efd4a0555cb9b86
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2022-11-07 15:26:52 -0600

    Enabling CORS for all domains

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,13 @@
 import * as express from "express";
 import * as passport from "passport";
 import * as path from "path";
+import * as cors from "cors";
 import routes from "./routes";
 import "./middlewares/passport.mw";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(routes);
